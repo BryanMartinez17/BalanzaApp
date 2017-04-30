aa4be5cd800e7d63f853e468f47c3b37791f41c3
BryanMartinez17 <bryanmartinezh17@gmail.com>
2017-04-30 21:30:03 +0200

    subida de proyecto

---
+++ Balanza/app/src/main/java/com/bryan/julio/balanza/Splash_Screen_Activity.java
@@ -0,0 +1,16 @@
+package com.bryan.julio.balanza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Screen_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        
+        // comentarios de ejemplo para subir algun cambio
+    }
+}
